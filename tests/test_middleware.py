2f6da97626be9c427dac0d5df69e6eae199e87f8
Tom Christie <tom@tomchristie.com>
2018-11-07 11:51:44 +0000

    Update tests to only deal with sync views on 3.7+

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -1,5 +1,6 @@
 import asyncio
 import random
+import sys
 
 import pytest
 from sentry_sdk import capture_message
@@ -15,8 +16,13 @@ from sentry_asgi import SentryMiddleware
 def app():
     app = Starlette(__name__)
 
-    @app.route("/message")
+    @app.route("/sync-message")
     def hi(request):
+        capture_message("hi", level="error")
+        return PlainTextResponse("ok")
+
+    @app.route("/async-message")
+    async def hi(request):
         capture_message("hi", level="error")
         return PlainTextResponse("ok")
 
@@ -36,13 +42,14 @@ def app():
     return app
 
 
-def test_request_data(sentry_init, app, capture_events):
+@pytest.mark.skipif(sys.version_info < (3, 7), reason="requires python3.7 or higher")
+def test_sync_request_data(sentry_init, app, capture_events):
     sentry_init()
     events = capture_events()
 
     with LifespanContext(app):
         client = TestClient(app)
-        response = client.get("/message?foo=bar")
+        response = client.get("/sync-message?foo=bar")
 
     assert response.status_code == 200
 
@@ -57,7 +64,40 @@ def test_request_data(sentry_init, app, capture_events):
         "user-agent",
     }
     assert event["request"]["query_string"] == "foo=bar"
-    assert event["request"]["url"].endswith("/message")
+    assert event["request"]["url"].endswith("/sync-message")
+    assert event["request"]["method"] == "GET"
+
+    # Assert that state is not leaked
+    events.clear()
+    capture_message("foo")
+    event, = events
+
+    assert "request" not in event
+    assert "transaction" not in event
+
+
+def test_async_request_data(sentry_init, app, capture_events):
+    sentry_init()
+    events = capture_events()
+
+    with LifespanContext(app):
+        client = TestClient(app)
+        response = client.get("/async-message?foo=bar")
+
+    assert response.status_code == 200
+
+    event, = events
+    assert event["transaction"] == "test_middleware.app.<locals>.hi"
+    assert event["request"]["env"] == {"REMOTE_ADDR": "testclient"}
+    assert set(event["request"]["headers"]) == {
+        "accept",
+        "accept-encoding",
+        "connection",
+        "host",
+        "user-agent",
+    }
+    assert event["request"]["query_string"] == "foo=bar"
+    assert event["request"]["url"].endswith("/async-message")
     assert event["request"]["method"] == "GET"
 
     # Assert that state is not leaked
